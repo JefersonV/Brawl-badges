6132d03a3bd6f11e450742e3b38cfe7b3bd0f7a3
JefersonV <jvelasquezc15@miumg.edu.gt>
2020-12-08 19:01:01 -0600

    init project

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render (){
+      return(
+				<React.Fragment>
+					<h1>Prueba</h1>
+				</React.Fragment>
+			); 
+    }
+}
+
+export default Navbar;
